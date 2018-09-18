69c425d087094da03f02a572ef4a0f870b945de9
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-18 20:49:11 -0300

    Classes anonimas

--- src/main/java/exemplos/java8/lambda/AppTest.java
+++ src/main/java/exemplos/java8/lambda/AppTest.java
@@ -2,29 +2,41 @@ package exemplos.java8.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
-	
+
 	private List<Filme> filmes;
 
 	@Before
-	public void setup(){
+	public void setup() {
 		Filme filme1 = new Filme("A Clockwork Orange", 8);
 		Filme filme2 = new Filme("2001: A Space Odyssey", 7);
 		Filme filme3 = new Filme("Full Metal Jacket", 6);
-		
-		filmes = Arrays.asList(filme1,filme2, filme3); 
+
+		filmes = Arrays.asList(filme1, filme2, filme3);
 	}
-	
+
 	@Test
-	public void testInterfaceConsumerComForEach(){
+	public void testInterfaceConsumerComForEach() {
 		ExibeNomeFilme consumidor = new ExibeNomeFilme();
 		filmes.forEach(consumidor);
 	}
-	
 
+	@Test
+	public void TestClasseAnonimaComForEach() {
+		Consumer<Filme> exibeNomeFilme = new Consumer<Filme>() {
+
+			@Override
+			public void accept(Filme filme) {
+				System.out.println(filme.getNome());
+			}
+		};
+		
+		filmes.forEach(exibeNomeFilme);
+	}
 
 }
