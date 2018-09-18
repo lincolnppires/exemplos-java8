3fdd643d3d56aa04b5c872bf351a0247de1ab95e
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-18 20:02:51 -0300

    Commit inicial

---
+++ src/main/java/br/com/lincoln/exemplos_java8/App.java
@@ -0,0 +1,13 @@
+package br.com.lincoln.exemplos_java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
