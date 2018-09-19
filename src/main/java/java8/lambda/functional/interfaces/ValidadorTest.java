3d3c1e037e8fe4084be9e198f983113736cdd3a0
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-19 10:54:26 -0300

    Teste interface funcional

--- src/main/java/java8/lambda/functional/interfaces/ValidadorTest.java
+++ src/main/java/java8/lambda/functional/interfaces/ValidadorTest.java
@@ -12,6 +12,8 @@ public class ValidadorTest {
 	public void testeValidadorCEP(){
 		Validador<String> validadorCEP = 
 				cep -> cep.matches("[0-9]{5}-[0-9]{3}");
+				
+		System.out.println(validadorCEP.getClass());		
 		
 		assertTrue(validadorCEP.valida("35400-000"));
 		assertFalse(validadorCEP.valida("35400-00A"));
