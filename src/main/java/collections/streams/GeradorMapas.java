21987191a7f492fe9b4dccded2ca95c005a159d7
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-20 15:17:23 -0300

    Exemplo uso partitioningBy

--- src/main/java/collections/streams/GeradorMapas.java
+++ src/main/java/collections/streams/GeradorMapas.java
@@ -20,10 +20,12 @@ public class GeradorMapas {
 	@Before
 	public void setup() {
 		Filme filme1 = new Filme("A Clockwork Orange", 9);
+		filme1.setCensurado(true);
 		Filme filme2 = new Filme("2001: A Space Odyssey", 7);
 		Filme filme3 = new Filme("Full Metal Jacket", 6);
 		Filme filme4 = new Filme("The Shining", 8);
 		Filme filme5 = new Filme("Lolita", 8);
+		filme5.setCensurado(true);
 		Filme filme6 = new Filme("2001: A Space Odyssey", 7);
 		filmes = Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6);
 	}
@@ -50,6 +52,28 @@ public class GeradorMapas {
 		System.out.println(mapaNotas);
 	}
 	
+	@Test
+	public void TestaMapFilmeParticionadoPorCensura(){
+		
+		Map<Boolean, List<Filme>> mapaCensuradoFilme = 
+				filmes.stream()
+					.collect(Collectors.partitioningBy(Filme::isCensurado));
+		
+		System.out.println(mapaCensuradoFilme);
+		
+	}
+	
+	@Test
+	public void TestaMapNomeFilmeParticionadoPorCensura(){
+		
+		Map<Boolean, List<String>> mapaCensuradoNomeFilme = 
+				filmes.stream()
+					.collect(Collectors
+								.partitioningBy(Filme::isCensurado, 
+										Collectors.mapping(Filme::getNome, Collectors.toList())));
+		
+		System.out.println(mapaCensuradoNomeFilme);
+	}
 
 }
 
