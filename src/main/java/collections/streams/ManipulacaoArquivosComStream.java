324e71004c48887d8d046b37230f9d5a1f58b622
Lincoln Pires <lincoln.ufop@gmail.com>
2018-09-20 14:33:29 -0300

    Exemplo uso toMap

--- src/main/java/collections/streams/ManipulacaoArquivosComStream.java
+++ src/main/java/collections/streams/ManipulacaoArquivosComStream.java
@@ -5,6 +5,9 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -36,7 +39,57 @@ public class ManipulacaoArquivosComStream {
 		System.out.println("Conteudo");
 		Files.list(Paths.get("./src/main/java/collections/streams"))
 			.filter(file -> file.toString().endsWith(".java"))
-			.flatMap(file -> lines(file))
-			.forEach(System.out::println);
+			.flatMap(file -> lines(file));
+			//.forEach(System.out::println);
 	}
+	
+	@Test
+	public void TestaNumeroDeLinhasPorArquivo() throws IOException{
+		System.out.println("Linhas por Arquivo");
+		
+		Map<Path, Long> linhasPorArquivo = new HashMap<>();
+		
+		Files.list(Paths.get("./src/main/java/collections/streams"))
+			.filter(file -> file.toString().endsWith(".java"))
+			.forEach(
+					file -> linhasPorArquivo.put(file, lines(file).count()));
+		System.out.println(linhasPorArquivo);
+		
+		//
+		Map<Path, Long> linhasPorArquivo2 = new HashMap<>();
+		
+		linhasPorArquivo2 = Files.list(Paths.get("./src/main/java/collections/streams"))
+			.filter(file -> file.toString().endsWith(".java"))
+			.collect(Collectors.toMap(file -> file, file -> lines(file).count()));
+		
+		System.out.println(linhasPorArquivo2);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
